0753dea84ff244d49cc73ae7108479e2c561aa83
rlw92 <r_l_williams@yahoo.com>
2022-03-20 09:49:50 +0000

    added the layout of the library and a bit of js.

---
+++ js.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+/*User interface*/
+
+
+
+function cons(){
+    let title = document.getElementById("title").value;
+let auth = document.getElementById("psw").value;
+let pgnum = document.getElementById("pgnum").value;
+let chec = document.getElementById("check");
+
+    console.log(title,auth,pgnum);
+}
