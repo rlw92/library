4cb42c656bf2771ada2b88db7ce01d1e804f92ef
rlw92 <r_l_williams@yahoo.com>
2022-03-21 12:51:02 +0000

    got the interface working to the point that the data is being updated on the screen, still a lot of work to do

--- js.js
+++ js.js
@@ -1,22 +1,79 @@
-let myLibrary = [];
+let myLibrary = [
 
-function Book() {
-  // the constructor...
+  {
+    title: "A Game of Thrones",
+    author: "George R. R. Martin",
+    pgnum: 694,
+    r: false
+  }
+];
+
+
+let titl = document.getElementById("title");
+let auth = document.getElementById("psw");
+let pgnu = document.getElementById("pgnum");
+let che = document.getElementById("check");
+let r;
+if(che.checked === true){r = "read"}
+else if(che.checked === false){r = "unread"}
+
+function Book(title,author,pgnum,r){
+  this.title = title
+  this.author = author
+  this.pgnum = pgnum
+  this.r = r
+
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = titl.value;
+  let author = auth.value;
+  let pgnum = pgnu.value;
+  let r = che;
+  let newbook = new Book(title,author,pgnum,r);
+  myLibrary.push(newbook);
+addcard(title,author);
+
+ }
+
+ function addcard(title,author){
+ let bookNode = document.createElement("div");
+
+let titleNode = document.createElement("h2");
+ titleNode.textContent =  title;
+
+ let authorNode = document.createElement("h2");
+ authorNode.textContent = author;
+
+ bookNode.appendChild(titleNode);
+ bookNode.appendChild(authorNode);
+
+ let maincontent = document.querySelector(".maincontent");
+ maincontent.appendChild(bookNode);
 }
 
-/*User interface*/
 
-function cons(){
-    let title = document.getElementById("title").value;
-let auth = document.getElementById("psw").value;
-let pgnum = document.getElementById("pgnum").value;
-let chec = document.getElementById("check");
-let r;
-if(chec.checked === true){r = "read"}
-else if(chec.checked === false){r = "unread"}
-    console.log(title,auth,pgnum,r);
-}
+ /*let authorNode = document.createElement("h3");
+ let pageNode = document.createElement("h3");
+ let readNode = document.createElement("h3");
+
+ 
+  bookNode.appendChild(titleNode);
+  bookNode.appendChild(authorNode);
+  bookNode.appendChild(pageNode);
+  bookNode.appendChild(readNode);
+*/
+  
+
+
+
+
+
+
+
+    for(i=0;i<myLibrary.length;i++){
+    
+    document.querySelector(".title").textContent = myLibrary[i].title;
+    document.querySelector(".author").textContent = myLibrary[0].author;
+    document.querySelector(".pages").textContent = myLibrary[0].pgnum;
+    document.querySelector(".read").textContent = myLibrary[0].r;}
